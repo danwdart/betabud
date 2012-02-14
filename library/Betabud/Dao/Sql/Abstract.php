af53af4899f2e78151a3b35d45105e4fdbf9ce42
Dan <git@dandart.co.uk>
2012-02-14 22:15:29 +0000

    start on the Sql Dao as an option

---
+++ library/Betabud/Dao/Sql/Abstract.php
@@ -0,0 +1,26 @@
+<?php
+class Betabud_Dao_Sql_Abstract
+{
+    private static %_zendDb;
+
+    public static function setAdapter(Zend_Db_Adapter_Abstract $_zendDb)
+    {
+        self::$_zendDb = $zendDb;
+    }
+
+    private static function _getAdapter()
+    {
+        if(!self::$_zendDb instanceof Zend_Db_Adapter_Abstract) {
+            throw new Betabud_Dao_Sql_Exception_NoAdapter();
+        }
+        return self::$_zendDb;
+    }
+
+    // Todo: cache if necessary
+    protected function _getConnection()
+    {
+        self::$_zendDb->getConnection();
+    }
+}
+
+
