510a09f5965f288485bf097a29b58db9ff11ebca
Dan <git@dandart.co.uk>
2012-02-18 17:26:25 +0000

    online heartbeats

--- js/server.js
+++ js/server.js
@@ -1,12 +1,24 @@
-var io = require('socket.io').listen(8080);
+var io = require('socket.io').listen(8080),
+    online = [];
 
 io.sockets.on('connection', function (socket) {
     console.log(socket);
+
     socket.emit('message', {from: 'system', text:'user connected'});
+    socket.on('heartbeat', function(identity) {
+        socket.set('nick', identity.name);
+        online[identity.name] = identity.name;
+        socket.emit('onlinepeople', online);
+    });
     socket.on('message', function(msg) {
         console.log(msg.from + ' said: '+msg.text);
         socket.emit('message', msg);
     });
     socket.on('disconnect', function() {
+        identity = socket.get('identity', function(err, nick) {
+            offline = {name: nick, status:'offline'};
+            socket.broadcast.emit('heartbeat', offline);
+            delete online[nick];
+        });
     });
 });
--- public/js/socket.js
+++ public/js/socket.js
@@ -1,10 +1,24 @@
-var socket = io.connect('http://betabud.dandart.co.uk:8080');
+var socket = io.connect('http://betabud.dandart.co.uk:8080'),
+    online = [];
+function updateonline() {
+    html = online.join('<br/>');
+    $('.people').html(html);
+}
 socket.on('connect', function() {
+    socket.emit('heartbeat', {name: betabud.nickname, status: 'online'});
     $('.chatlog').html('');
 });
 socket.on('message', function(data) {
     html = $('.chatlog').html();
     $('.chatlog').html(html + data.from + ' said: '+data.text+'<br/>');
+});
+socket.on('heartbeat', function(data) {
+    online[data.name] = data.name;
+    updateonline();
+});
+socket.on('onlinepeople', function(data) {
+    online = data;
+    updateonline();
 });
 $('#form #submit').click(function(event) {
     event.preventDefault();
