bf09f05e5296061ae0b5358c3a8524b7610613c6
Dan <git@dandart.co.uk>
2012-02-18 10:45:01 -0600

    differ to write to clients

--- js/server.js
+++ js/server.js
@@ -5,7 +5,7 @@ io.sockets.on('connection', function (socket) {
     socket.emit('message', {from: 'system', text:'user connected'});
     socket.on('message', function(msg) {
         console.log(msg.from + ' said: '+msg.text);
-        socket.emit('message', msg);
+        socket.broadcast.emit('message', msg);
     });
     socket.on('disconnect', function() {
     });
--- public/js/socket.js
+++ public/js/socket.js
@@ -1,15 +1,20 @@
+function writemessage(data) {
+    html = $('.chatlog').html();
+    $('.chatlog').html(html + data.from + ' said: '+data.text+'<br/>');
+}
 var socket = io.connect('http://betabud.dandart.co.uk:8080');
 socket.on('connect', function() {
     $('.chatlog').html('');
 });
 socket.on('message', function(data) {
-    html = $('.chatlog').html();
-    $('.chatlog').html(html + data.from + ' said: '+data.text+'<br/>');
+    writemessage(data);
 });
 $('#form #submit').click(function(event) {
     event.preventDefault();
     text = $('#chatline').val();
     $('#chatline').val('');
-    socket.emit('message',{from: betabud.nickname, text:text});
+    msg = {from:betabud.nickname, text:text};
+    socket.emit('message',msg);
+    writemessage(msg);
     return false;
 });
